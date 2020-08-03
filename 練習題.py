d2b94be353fc76e7cd7ed08ec73565b213696f43
20080603 <69131590+20080603@users.noreply.github.com>
2020-08-03 16:08:35 +0800

    Add files via upload

---
+++ 練習題.py
@@ -0,0 +1,22 @@
+a=3
+b=1
+print("a+b")
+a=10
+a+1
+print(a)
+x=input("輸入一個數字")
+x=int(x)
+print(x/2)
+
+
+if True:
+    print('A')
+else:
+    print('B')
+    print('C')
+    
+if False:
+    print('A')
+else:
+    print('B')
+    print('C')

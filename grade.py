a91fe2b5abc8c958072b5cccb9f6a652c5bf05a9
20080603 <69131590+20080603@users.noreply.github.com>
2020-08-03 14:42:00 +0800

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,12 @@
+r=int (input("請輸入成績:"))
+if r>=90:
+    print("分數等極為A")
+elif r>=80:
+    print("分數等極為B")
+elif r>=70:
+    print("分數等極為C")
+elif r>=60:
+    print("分數等極為D")
+else:
+    print("分數等極為E")
+

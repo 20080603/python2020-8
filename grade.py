42d69f766b40e2f541370f19f706fb7e6886477f
20080603 <69131590+20080603@users.noreply.github.com>
2020-08-03 15:25:57 +0800

    Add files via upload

--- grade.py
+++ grade.py
@@ -9,4 +9,17 @@ elif r>=60:
     print("分數等極為D")
 else:
     print("分數等極為E")
+    
+    
+    
+r=int (input("請輸入成績1:"))
+s=int (input("請輸入成績2:"))
+if r>=90 and s>=90 or r>=100 and s>=90 or r>=90 and s>=100:
+    print("有獎品")
+elif r>=90 and s<90 or r<=90 and s>90 or r>=80 and s>=80:
+    print("再加油")
+elif r<=60 and s<=60:
+    print("要處罰")
+else:
+    print("分數過低")
 

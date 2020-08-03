75ac0a9d64bb9a8009cc73920a4da37bfb75d66d
20080603 <69131590+20080603@users.noreply.github.com>
2020-08-03 16:50:02 +0800

    Add files via upload

--- 猜數字.py
+++ 猜數字.py
@@ -35,6 +35,23 @@ while True:
         print("猜錯了")
     print(num)
     
+import random
+a = 1
+b = 100
+num = random.randint(a,b)
+while True:
+    print("目前範圍%d-%d"%(a,b))
+    r =  int(input('輸入數字:'))
+    if r<a or r>b:
+        print("請重新輸入數字")
+    elif r>num:
+        b = r
+    elif r<num:
+        a = r
+    elif r==num:
+        print("加分")
+        break
+    
     
 
 
